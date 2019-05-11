843183383fcc0e4c8fa71e6b8781a2aeecdbaea2
oahmaro <osamaahmaro@gmail.com>
2019-05-11 12:04:40 +0300

    Parent Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,12 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
   render() {
+    const { tweet } = this.props
+
+    if (tweet === null) {
+      return <p>This Tweet doesn't exist</p>
+    }
+
     console.log(this.props)
     return <div className="tweet" />
   }
@@ -11,9 +17,12 @@ class Tweet extends Component {
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
   const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
   }
 }
 
